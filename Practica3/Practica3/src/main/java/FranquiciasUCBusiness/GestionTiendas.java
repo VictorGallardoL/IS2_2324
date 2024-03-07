4ab904c07983c97b61bf9fb19c5893c58d93321b
VictorGallardoL <vgl946@alumnos.unican.es>
2024-03-07 11:59:52 +0100

    Añadir clases

---
+++ Practica3/Practica3/src/main/java/FranquiciasUCBusiness/GestionTiendas.java
@@ -0,0 +1,21 @@
+public class GestionTiendas implements IGestionTiendas {
+
+	@Override
+	public Tienda nuevaTienda(Tienda t) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tienda eliminarTienda(String nombre) throws OperacionNoValidaException, DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tienda tienda(String nombre) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
